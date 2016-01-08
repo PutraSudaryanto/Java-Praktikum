a1616a020fa68f1f80f8ea8d3a7ac9e7bb110163
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-01-08 17:15:02 +0700

    pertemuan 8, soal 1-4

---
+++ p8/DelapanEmpat.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+
+public class DelapanEmpat extends Frame {
+    public DelapanEmpat() {
+        setTitle("Contoh Button");
+        setLayout(new FlowLayout());
+        add(new Button("yellow"));
+        add(new Button("blue"));
+        add(new Button("orange"));
+        add(new Button("cyan"));
+        add(new Button("pink"));
+        add(new Button("red"));
+        add(new Button("white"));
+    }
+
+    public boolean action(Event evt, Object arg) {
+        if(arg.equals("yellow"))
+            setBackground(Color.yellow);
+        else if(arg.equals("blue"))
+            setBackground(Color.blue);
+        else if(arg.equals("orange"))
+            setBackground(Color.orange);
+        else if(arg.equals("cyan"))
+            setBackground(Color.cyan);
+        else if(arg.equals("pink"))
+            setBackground(Color.pink);
+        else if(arg.equals("red"))
+            setBackground(Color.red);
+        else if(arg.equals("white"))
+            setBackground(Color.white);
+        else
+            return false;
+
+        repaint();
+        return true;
+    }
+
+    public static void main(String args[]) {
+        DelapanEmpat f = new DelapanEmpat();
+        f.setLocation(300,100);
+        f.resize(400,300);
+        f.show();
+    }
+}
