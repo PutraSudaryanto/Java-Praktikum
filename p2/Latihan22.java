9da3648da8c15e794ea635137c968d69fd696f26
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-01-08 17:53:09 +0700

    pertemuan 2: fixed

---
+++ p2/Latihan22.java
@@ -0,0 +1,16 @@
+public class Latihan22 {
+	public static void main(String[] args) {
+		int[][] x = new int[3][2];
+		x[0][0] = 7;  x[0][1] = 3;
+		x[1][0] = 5;  x[1][1] = 6;
+		x[2][0] = 1;  x[2][1] = 0;
+
+		System.out.print("x[0][0] : "+ x[0][0] +"  | ");
+		System.out.print("x[0][1] : "+ x[0][1] +"  \n");
+		System.out.print("x[1][0] : "+ x[1][0] +"  | ");
+		
+		System.out.print("x[1][1] : "+ x[1][1] +"  \n");
+		System.out.print("x[2][0] : "+ x[2][0] +"  | ");
+		System.out.print("x[2][1] : "+ x[2][1] +"  \n");
+	}
+}
---
+++ p2/Latihan23.java
@@ -0,0 +1,15 @@
+public class Latihan23 {
+	public static void main(String[] args) {
+		int[] a = new int[1];
+		int[] b = new int[1];
+		int[] c = new int[1];
+		int[] d = new int[1];
+		a[0] = 2;
+		b[0] = 3;
+		c[0] = a[0]+b[0];
+		d[0] = a[0]*b[0];
+
+		System.out.print("penjumlahan a[] + b[] adalah " + c[0] + " \n");
+		System.out.print("perkalian a[] * b[] adalah " + d[0] + " \n");
+	}
+}
