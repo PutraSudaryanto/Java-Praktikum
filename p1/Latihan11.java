4f44f45aeb303d1238f47d1f7d0ed467cc5a2994
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-01-08 23:44:45 +0700

    pertemuan 1: fixed

---
+++ p1/Latihan11.java
@@ -0,0 +1,8 @@
+public class Latihan11 {
+	public static void main (String[] args) {
+		System.out.println("nama : Dwi Putra Sudaryanto"); 
+		System.out.println("nim : 12111075");
+		System.out.println("hobi : Coding");
+		System.out.println("Saya belajar bahasa java karena ingin mendalami proses pembuatan aplikasi android yang dikembangkan menggunakan java");
+	}
+}
